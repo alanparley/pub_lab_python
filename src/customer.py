80b5b8b16bdf9f1a6debc37e1ae5d6d480e27f29
Alan Parley <alanparley@hotmail.com>
2020-09-16 13:51:26 +0100

    added methods to pub.py

--- src/customer.py
+++ src/customer.py
@@ -2,3 +2,9 @@ class Customer:
     def __init__(self, name, cash):
         self.name = name
         self.cash = cash
+        self.drinks = []
+
+# make a method for the customer to buy a drink from the pub
+
+    def cust_buys_drink(self):
+        pass
--- src/pub.py
+++ src/pub.py
@@ -3,3 +3,12 @@ class Pub:
         self.name = name
         self.cash = cash
         self.drink = []
+
+    def add_drinks(self, drink):
+        self.drink.append(drink)
+
+    def count_drinks(self, drink):
+        return len(self.drink)
+
+    def sell_drink(self, drink):
+        self.drink.remove(drink)
